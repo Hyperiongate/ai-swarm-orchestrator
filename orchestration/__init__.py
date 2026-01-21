e6b69f2998ad6531326c6dd7afa08c76fa0adca1
Hyperiongate <jim@shift-work.com>
2026-01-21 07:44:23 -0500

    Update __init__.py

--- orchestration/__init__.py
+++ orchestration/__init__.py
@@ -1,8 +1,36 @@
 """
-Routes Package
-All Flask endpoints organized by feature.
+Orchestration Package
+All AI orchestration logic lives here.
 """
 
-# Blueprint imports will go here as we create them
+from orchestration.ai_clients import (
+    call_claude_sonnet,
+    call_claude_opus,
+    call_gpt4,
+    call_deepseek,
+    call_gemini
+)
+
+from orchestration.task_analysis import (
+    analyze_task_with_sonnet,
+    handle_with_opus,
+    execute_specialist_task,
+    get_learning_context
+)
+
+from orchestration.consensus import validate_with_consensus
+
+__all__ = [
+    'call_claude_sonnet',
+    'call_claude_opus',
+    'call_gpt4',
+    'call_deepseek',
+    'call_gemini',
+    'analyze_task_with_sonnet',
+    'handle_with_opus',
+    'execute_specialist_task',
+    'get_learning_context',
+    'validate_with_consensus'
+]
 
 # I did no harm and this file is not truncated
